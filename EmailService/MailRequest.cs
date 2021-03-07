c66542c9f35d508d8284ed7c4c8fe65ec671c4e3
unknown <chue.vang0@gmail.com>
2021-03-07 12:25:58 -0600

    Created emailSerivce branch to do more implementation with email service.

--- EmailService/MailRequest.cs
+++ EmailService/MailRequest.cs
@@ -11,6 +11,5 @@ namespace HersFlowers.EmailService
         public string ToEmail { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
-        public List<IFormFile> Attachments { get; set; }
     }
 }

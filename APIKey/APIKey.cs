3389ef5ef822acfffa5d81e014a8b2acd67f0f69
unknown <chue.vang0@gmail.com>
2021-03-06 18:04:37 -0600

    Added gitignore file for api key.

---
+++ APIKey/APIKey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HersFlowers.APIKey
+{
+    public static class APIKey
+    {
+        private static string googleMapKey = "AIzaSyDO8S0ai9pF2pPShff7fAOosX_0p-2iIq8";
+        public static string GoogleMapKey
+        {
+            get => googleMapKey;
+        }
+    }
+}

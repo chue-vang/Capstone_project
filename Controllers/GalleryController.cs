22d0c0cd7b1c8c45de248719c2672d11631ecbe5
unknown <chue.vang0@gmail.com>
2021-03-08 19:24:56 -0600

    Finish implementing a photo gallery using a lightbox.  An owner is also able to successfully upload images to a folder on wwwroot.

--- Controllers/GalleryController.cs
+++ Controllers/GalleryController.cs
@@ -22,5 +22,36 @@ namespace HersFlowers.Controllers
             viewModel.image = new Image();
             return View(viewModel);
         }
+
+        [HttpPost]
+        public IActionResult AddPhotos(ImageViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var Files = model.image.filePhoto;
+            if (Files.Count > 0)
+            {
+                foreach (var item in Files)
+                {
+                    Image image = new Image();
+                    var guid = Guid.NewGuid().ToString();
+                    var filePath = "wwwroot/photos/" + guid + item.FileName;
+                    var fileName = guid + item.FileName;
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        item.CopyTo(stream);
+                        image.Name = fileName;
+                        image.Path = filePath;
+                        image.Title = item.FileName;
+                        image.NoOfViews = 1;
+                        _context.Images.Add(image);
+                        _context.SaveChanges();
+                    }
+                }
+                return RedirectToAction("Index", "Gallery");
+            }
+            return RedirectToAction("Index", "Gallery");
+        }
     }
 }
